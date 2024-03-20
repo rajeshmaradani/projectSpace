6c14d3d8edd275d97a76f209c425f9a9f2a5c080
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2024-03-20 10:00:38 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const SETTINGS = {
-    "CONVAI-API-KEY": "b271161992d0d6b013b01ce15a0cafb3",
-    "CHARACTER-ID": "45bade8e-e49f-11ee-9403-42010a7be009"
+    "CONVAI-API-KEY": "3855ae9599bd6cab7b4ec20860530608",
+    "CHARACTER-ID": "fffa71bc-e5e7-11ee-b0aa-42010a7be00e"
 };
